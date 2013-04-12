b9625958fa24c9c7f1a75b308ee645d167367fa2
arkadiusz <arkadiusz.wisniewski@barrelny.com>
2013-04-12 11:43:47 -0400

    Db creds update.

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'deirdre';
+$db['default']['password'] = 'GNBWTA10';
 $db['default']['database'] = 'checklistpup';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
